256f77e1fe76f48ff04d2d2603c9d0c191569ee3
johnwojo <jwojo13@gmail.com>
2018-04-06 19:08:52 -0400

    Added delete functionality to tasks.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,62 +16,73 @@ class TasksController < ApplicationController
     end
   end
 
-    post '/tasks' do
-      if logged_in?
-        if params[:name] == ""
-          redirect to '/tasks/new'
-        else
-          @task = Task.new(name: params[:name],description: params[:description])
-          if @task.save
-            redirect to "/tasks/#{@task.id}"
-          else
-            redirect to '/tasks/new'
-          end
-        end
+  post '/tasks' do
+    if logged_in?
+      if params[:name] == ""
+        redirect to '/tasks/new'
       else
-        redirect to "/login"
-      end
-    end
-
-
-    get '/tasks/:id' do
-      if logged_in?
-        @task = Task.find_by_id(params[:id])
-        erb :'/tasks/show_task'
-      else
-        redirect to '/login'
-      end
-    end
-
-
-    get '/tasks/:id/edit' do
-      if logged_in?
-        @task = Task.find_by_id(params[:id])
-        if @task
-          # && @city.user == current_user
-          erb :'tasks/edit_task'
-        else
-          redirect to '/tasks'
-        end
-      else
-        redirect to '/login'
-      end
-    end
-
-
-    patch '/tasks/:id' do
-      if logged_in?
-        if params[:name] == ""
-          redirect to "/tasks/#{params[:id]}/edit"
-        else
-          @task = Task.find_by_id(params[:id])
-          @task.name = params[:name]
-          @task.description = params[:description]
-          @task.save
+        @task = Task.new(name: params[:name],description: params[:description])
+        if @task.save
           redirect to "/tasks/#{@task.id}"
+        else
+          redirect to '/tasks/new'
         end
-      else
-        redirect to '/login'
       end
+    else
+      redirect to "/login"
     end
+  end
+
+  get '/tasks/:id' do
+    if logged_in?
+      @task = Task.find_by_id(params[:id])
+      erb :'/tasks/show_task'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/tasks/:id/edit' do
+    if logged_in?
+      @task = Task.find_by_id(params[:id])
+      if @task
+        # && @city.user == current_user
+        erb :'tasks/edit_task'
+      else
+        redirect to '/tasks'
+      end
+    else
+      redirect to '/login'
+    end
+  end
+
+  patch '/tasks/:id' do
+    if logged_in?
+      if params[:name] == ""
+        redirect to "/tasks/#{params[:id]}/edit"
+      else
+        @task = Task.find_by_id(params[:id])
+        @task.name = params[:name]
+        @task.description = params[:description]
+        @task.save
+        redirect to "/tasks/#{@task.id}"
+      end
+    else
+      redirect to '/login'
+    end
+  end
+
+
+  delete '/tasks/:id/delete' do
+    if logged_in?
+      @task = Task.find_by_id(params[:id])
+      if @task
+        #  && @city.user == current_user
+        @task.delete
+      end
+      redirect to '/tasks/'
+    else
+      redirect to '/login'
+    end
+  end
 end
