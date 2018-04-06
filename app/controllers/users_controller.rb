551e2c2676488d0c4fc09a24a2e2425211118c5c
johnwojo <jwojo13@gmail.com>
2018-04-06 19:12:36 -0400

    Spacing and indentation.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,32 +25,31 @@ class UsersController < ApplicationController
     end
   end
 
-
-    get '/login' do
-      if logged_in?
-        erb :'users/show'
-      else
-      erb :'users/login'
-      end
+  get '/login' do
+    if logged_in?
+      erb :'users/show'
+    else
+    erb :'users/login'
     end
+  end
 
-    post '/login' do
-      @user = User.find_by(:username => params[:username])
-      if @user && @user.authenticate(params[:password])
-        session[:user_id] = @user.id
-        erb :'users/show'
-      else
-        redirect to "/signup"
-      end
+  post '/login' do
+    @user = User.find_by(:username => params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      erb :'users/show'
+    else
+      redirect to "/signup"
     end
+  end
 
-    get '/logout' do
-      if logged_in?
-        session.destroy
-        redirect to "/login"
-      else
-        redirect to "/"
-      end
+  get '/logout' do
+    if logged_in?
+      session.destroy
+      redirect to "/login"
+    else
+      redirect to "/"
     end
+  end
 
 end
