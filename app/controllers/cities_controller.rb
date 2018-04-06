495dab4ade8148db4e66cd066c859b54eff57631
johnwojo <jwojo13@gmail.com>
2018-04-06 16:57:42 -0400

    Adedd cities/:id functionality. Edited create_city.erb.

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,20 +1,46 @@
 class CitiesController < ApplicationController
-
-    get '/cities' do
-      if logged_in?
-        @cities = City.all
-      erb :'cities/cities'
-      else
-        redirect to '/login'
-      end
+  get '/cities' do
+    if logged_in?
+      @cities = City.all
+    erb :'cities/cities'
+    else
+      redirect to '/login'
     end
+  end
 
-    get '/cities/new' do
-      if logged_in?
-        erb :'cities/create_city'
-      else
-        redirect to '/login'
-      end
+  get '/cities/new' do
+    if logged_in?
+      erb :'cities/create_city'
+    else
+      redirect to '/login'
     end
+  end
+
+post '/cities' do
+    if logged_in?
+      if params[:name] == ""
+        redirect to '/cities/new'
+      else
+        @city = current_user.cities.build(name: params[:name])
+        if @city.save
+          redirect to "/cities/#{@city.id}"
+        else
+          redirect to '/cities/new'
+        end
+      end
+    else
+      redirect to "/login"
+    end
+  end
+
+
+  get '/cities/:id' do
+    if logged_in?
+      @city = City.find_by_id(params[:id])
+      erb :'/cities/show_city'
+    else
+      redirect to '/login'
+    end
+  end
 
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,19 @@
 class TasksController < ApplicationController
+  get '/tasks' do
+    if logged_in?
+      @tasks = Task.all
+    erb :'tasks/tasks'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/tasks/new' do
+    if logged_in?
+      erb :'tasks/create_task'
+    else
+      redirect to '/login'
+    end
+  end
+
 end
