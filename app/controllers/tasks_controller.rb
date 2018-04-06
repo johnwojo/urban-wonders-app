787da56c25fc0adc0bacad2884612b3e0bc5d207
johnwojo <jwojo13@gmail.com>
2018-04-06 17:41:23 -0400

    Copied logic from cities to tasks.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,4 +16,66 @@ class TasksController < ApplicationController
     end
   end
 
+
+    post '/tasks' do
+      if logged_in?
+        if params[:name] == ""
+          redirect to '/tasks/new'
+        else
+          @task = current_user.tasks.build(name: params[:name],description: params[:description)
+          if @task.save
+            redirect to "/tasks/#{@task.id}"
+          else
+            redirect to '/tasks/new'
+          end
+        end
+      else
+        redirect to "/login"
+      end
+    end
+
+
+    get '/tasks/:id' do
+      if logged_in?
+        @task = Task.find_by_id(params[:id])
+        erb :'/tasks/show_city'
+      else
+        redirect to '/login'
+      end
+    end
+
+
+    get '/tasks/:id/edit' do
+      if logged_in?
+        @task = Task.find_by_id(params[:id])
+        if @task
+          # && @city.user == current_user
+          erb :'cities/edit_task'
+        else
+          redirect to '/tasks'
+        end
+      else
+        redirect to '/login'
+      end
+    end
+
+
+    patch '/tasks/:id' do
+      if logged_in?
+        if params[:name] == ""
+          redirect to "/tasks/#{params[:id]}/edit"
+        else
+          @task = Task.find_by_id(params[:id])
+          @task.name = params[:name]
+          @task.description = params[:description]
+          @task.save
+          redirect to "/cities/#{@task.id}"
+        end
+      else
+        redirect to '/login'
+      end
+    end
+
+
+
 end
