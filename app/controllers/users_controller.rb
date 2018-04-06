45f9e5427e1b3472fdbddd48aa4d1fa4932ef1cb
johnwojo <jwojo13@gmail.com>
2018-04-06 16:04:03 -0400

    Creating get and post for signup. Added users/:slug route as well.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+
+  get 'users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'users/show'
+  end
+
+  get 'signup' do
+    if logged_in?
+      erb :'users/show'
+    else
+      erb :'users/create_user'
+    end
+  end
+
+  post 'signup' do
+    if params[:username] == "" || params[:password] == "" || params[:email] == ""
+    redirect to "/signup"
+    else
+      @user = User.new(:username => params[:username], :password => params[:password], :email => params[:email])
+      @user.save
+      session[:user_id] = @user.id
+      erb :'users/show'
+    end
+  end
 end
