65d79d7b2bfc77fac1ceecaa45dacbfa99721af8
johnwojo <jwojo13@gmail.com>
2018-04-06 16:10:54 -0400

    Added login get and post and logout get.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,4 +23,33 @@ class UsersController < ApplicationController
       erb :'users/show'
     end
   end
+
+
+    get '/login' do
+      if logged_in?
+        erb :'users/show'
+      else
+      erb :'users/login'
+      end
+    end
+
+    post '/login' do
+      @user = User.find_by(:username => params[:username])
+      if @user && @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        erb :'users/show'
+      else
+        redirect to "/signup"
+      end
+    end
+
+    get '/logout' do
+      if logged_in?
+        session.destroy
+        redirect to "/login"
+      else
+        redirect to "/"
+      end
+    end
+
 end
