68b4d69d9d4dc7c8cc4cb2bccc462a2b8ac9957c
johnwojo <jwojo13@gmail.com>
2018-04-06 15:22:25 -0400

    Added has_secure_password into user model. Created hepler methods logged_in and current_user in the Application Controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,9 +8,19 @@ class ApplicationController < Sinatra::Base
     enable :sessions
     set :session_secret, "supersecret"
   end
-  
+
   get '/' do
     erb :index
+  end
+
+  helpers do
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
   has_many :user_cities
   has_many :cities, through: :user_cities
+  has_secure_password
+
+  def slug
+    username.gsub(" ","-").downcase
+  end
+
+  def self.find_by_slug(slug)
+    User.all.find{|user| user.slug == slug }
+  end
 end
